50e4b8322d482658ec38e7c62cece4413b685de6
Diwas777 <poudel.diwas777@gmail.com>
2021-10-14 12:20:03 +0545

    d

--- PracticeReset/Program.cs
+++ PracticeReset/Program.cs
@@ -10,7 +10,7 @@ namespace PracticeReset
     {
         static void Main(string[] args)
         {
-            Console.Write("Hellodds");
+            Console.Write("Helloddsd");
         }
     }
 }
